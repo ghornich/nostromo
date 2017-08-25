9f946d9287a6dc9125d5d00ffac9a4b23d2b79fb
Gergely Hornich <hornich.geri@gmail.com>
2017-08-25 17:23:28 +0200

    Fix buffer-image-search unit tests

--- modules/buffer-image-search/test/buffer-image-search.test.js
+++ modules/buffer-image-search/test/buffer-image-search.test.js
@@ -3,27 +3,20 @@ var pngjs=require('pngjs')
 var fs=require('fs')
 var resolve=require('path').resolve
 
-var bis=require('../')
+var bufferImageSearch=require('../')
 
-// var testImg0=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'test0.png')))
 var testImg1=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'galaxy1.png')))
 var testImg2=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'galaxy2.png')))
 var testImg3=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'galaxy3.png')))
 
-// var marker0=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'marker0.png')))
 var marker1=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'marker1.png')))
 var marker2=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'marker2.png')))
 var marker3=pngjs.PNG.sync.read(fs.readFileSync(resolve(__dirname, 'marker3.png')))
 
 test(t=>{
-    // var results0 = bis(testImg0, marker0)
-    var results1 = bis(testImg1, marker1)
-    var results2 = bis(testImg2, marker2)
-    var results3 = bis(testImg3, marker3)
-
-    // t.deepEqual(results0, [
-    //     { x: 2, y: 2 }
-    // ])
+    var results1 = bufferImageSearch(testImg1, marker1)
+    var results2 = bufferImageSearch(testImg2, marker2)
+    var results3 = bufferImageSearch(testImg3, marker3)
 
     t.deepEqual(results1, [
         { x: 0, y: 0 },
