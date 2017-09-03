c29e8302e65e826cbd26592d1095d15388eb92b1
Gergely Hornich <hornich.geri@gmail.com>
2017-09-03 21:25:55 +0200

    Add default-before-after test

---
+++ test/testrunner/default-before-after/default-before-after2.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports=module.exports=function(test){
+    test('default before/after commands 3', async t=>{
+        await t.click('.e')
+        return t.click('.f')
+    })
+    test('default before/after commands 4', async t=>{
+        await t.click('.g')
+        return t.click('.h')
+    })
+}
---
+++ test/testrunner/default-before-after/nostromo.run.conf.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const pathlib = require('path');
+const browserBounds = { size: { width: 800, height: 600 }, position: { x: 10, y: 10 } };
+
+exports = module.exports = function (config) {
+    return {
+        logLevel: config.LOG_LEVELS.ALL,
+        defaultBeforeCommand: function (t) {
+            t.comment('  -- defaultBeforeCommand')
+        },
+
+        defaultAfterCommand: function (t) {
+            t.comment('  -- defaultAfterCommand')
+        },
+
+        defaultBeforeTest: function (t) {
+            t.comment('  -- defaultBeforeTest')
+        },
+
+        defaultAfterTest: function (t) {
+            t.comment('  -- defaultAfterTest')
+        },
+
+        appUrl: `file:///${pathlib.resolve(__dirname, 'test.html')}`,
+
+        browsers: [
+            new config.browsers.Chrome({
+                name: 'Chrome',
+                path: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+                bounds: browserBounds,
+            }),
+            new config.browsers.Firefox({
+                name: 'Firefox',
+                path: 'C:/Program Files (x86)/Mozilla Firefox/firefox.exe',
+                bounds: browserBounds,
+            }),
+        ],
+        testFiles: ['default-before-after1.test.js', 'default-before-after2.test.js'],
+    };
+};
