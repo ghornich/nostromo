cfaba755f77bcc4d300450618ee6cb8828ca55dc
Gergely Hornich <hornich.geri@gmail.com>
2017-09-04 14:28:15 +0200

    Unfinished selector-observer module

---
+++ modules/selector-observer/selector-observer.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var assert=require('assert')
+
+exports = module.exports = SelectorObserver;
+
+/**
+ * @param {Object} conf
+ * @param {String} conf.selector
+ * @param {String} conf.onSelectorBecameVisible
+ */
+function SelectorObserver(conf) {
+	assert(typeof conf==='object','conf is not an object')
+
+	this._conf = conf
+
+	if ('MutationObserver' in window) {
+		var mo = new MutationObserver(this._onMutation.bind(this))
+		mo.observe(document.body, {childList:true,subtree:true,attributeFilter:['style','class']})
+	}
+	else {
+		// TODO implement polling?
+		throw new Error('MutationObserver not supported')
+	}
+}
+
+SelectorObserver.prototype._onMutation=function(me){
+
+}
