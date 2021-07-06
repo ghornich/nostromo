aacf52654a03cb4c7dc60ab8a8bc902dd674ec1c
Gergely Hornich <ghornich@users.noreply.github.com>
2021-07-06 19:30:57 +0200

    Update ts-dist file

--- ts-dist/test/self-tests/test-testapp.js
+++ ts-dist/test/self-tests/test-testapp.js
@@ -2,11 +2,11 @@
 exports = module.exports = function (test) {
     test('test-testapp', async (t) => {
         await t.click('#show-dialog');
-        await t.assert();
+        await t.screenshot();
         await t.setValue('#input', 'A');
         await t.click('#add-btn');
-        await t.screenshot('#add-btn');
+        await t.screenshot('#list');
         await t.click('.delete');
-        await t.assert();
+        await t.screenshot();
     });
 };
