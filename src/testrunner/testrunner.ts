715ea210ce9e5293749e3f5bf5039995edd684d2
Gergely Hornich <ghornich@users.noreply.github.com>
2023-01-17 21:42:31 +0100

    Fix setFileInput

--- src/testrunner/testrunner.ts
+++ src/testrunner/testrunner.ts
@@ -735,8 +735,8 @@ export default class Testrunner extends EventEmitter {
                     }
                 }
 
-                // @ts-expect-error FIXME implementation leak, don't use _page, maybe move setFileInput to BrowserInterface
-                const fileChooserPromise = this._currentBrowser._page.waitForFileChooser();
+                // @ts-expect-error FIXME implementation leak, don't use getPage, maybe move setFileInput to BrowserInterface
+                const fileChooserPromise = (await this._currentBrowser.getPage()).waitForFileChooser();
                 await this._currentBrowser.click(selector);
                 await (await fileChooserPromise).accept([filePath]);
             }, []);
