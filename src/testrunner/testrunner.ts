be6e95dea487be0fd4da435a5e8db48c2bf98627
Gergely Hornich <ghornich@users.noreply.github.com>
2023-12-08 20:48:04 +0100

    Remove unnecessary logs

--- src/testrunner/testrunner.ts
+++ src/testrunner/testrunner.ts
@@ -204,7 +204,6 @@ export default class Testrunner extends EventEmitter {
         catch (error) {
             this.setExitCode(1);
             this._log.error(error);
-            this._log.error(error.stack);
         }
         finally {
             this._log.info(`Finished in ${prettyMs(Date.now() - runStartTime, { verbose: true })}`);
@@ -416,7 +415,6 @@ export default class Testrunner extends EventEmitter {
         }
         catch (error) {
             this._log.error(`browser "${browser.name}" failed to start (${error})`);
-            this._log.error(error.stack);
             throw error;
         }
 
